83af11e0e88131c497a1f80618f1904d7c73ad4e
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-03-18 09:11:31 -0600

    more js and css

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function startQuiz() {
   presentQuestion()
 }
 
-// Creates timer and links to html through timerElement variable
+// Creates timer and links to html through timerCount
 function startTimer() {
   timer = setInterval(function() {
     timerCount--;
@@ -50,27 +50,54 @@ function startTimer() {
   }, 1000);
 }
 
-// Set correct answer coounter
-function correct() {
-  correctCounter++;
-  startButtton.disabled = false;
-  setcorrectCounter()
-}
-
-// Set wrong answer counter
-function wrong() {
-  wrongCounter++;
-  startButtton.disabled = false;
-  setwrongCounter()
-}
-
-
 // Function to link chosen question to html
 function presentQuestion() {
 
 }
 
-// Need a function to enter initials, score, and save to local storage
+// I need to determine if the answer is correct or wrong
+function answerCorrect() {
+
+}
+
+function answerWrong() {
+
+}
+// Set correct answer counter
+function correct() {
+  correct.textContent = correctCounter;
+  localStorage.setItem("correctCount", correctCounter);
+}
+
+// Set wrong answer counter
+function wrong() {
+  wrong.textContent = wrongCounter;
+  localStorage.setItem("wrongCount", wrongCounter);
+}
+// Get stored values for correct and incorrect
+function getCorrect() {
+  var storedCorrect = localStorage.getItem("correctCount");
+  
+  if (storedCorrect === null) {
+    correctCounter = 0;
+  } else {
+    correctCounter = storedCorrect;
+  }
+  correct.textContent = correctCounter
+}
+
+function getWrong() {
+  var storedWrong = localStorage.getItem("wrongCount");
+  if (storedWrong === null) {
+    wrongCounter = 0;
+  } else {
+    wrongCounter = storedWrong;
+  }
+  wrong.textContent = wrongCounter;
+}
+
+
+// Need a prompt?? to enter initials, score, and save to local storage
 
  startButtton.addEventListener("click", startQuiz);
 
